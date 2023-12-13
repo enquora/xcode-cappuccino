bdbead71b78e1e7a88dc2888baa80baac5238969
David Richardson <david.richardson@enquora.com>
2023-12-12 17:25:06 -0700

    Update plugin compatibility UUID for Xcode 15.1 Update install procedure to no longer install xclangspec and language metadata file to Xcode bundle - not required, and it now causes Xcode to crash.

--- References.md
+++ References.md
@@ -1,4 +1,18 @@
 #  References
 
 https://pewpewthespells.com/blog/xcode_build_system.html
+http://maxao.free.fr/xcode-plugin-interface/specifications.html
+https://benclarkxyz.github.io/blog/programming/xcode/index.html
+
+
+Xcode LSP and Source Editor Extensions
+* https://swiftrocks.com/code-generation-with-sourcekit
+* https://github.com/apple/sourcekit-lsp
+* https://github.com/apple/sourcekit-lsp/blob/main/Documentation/Development.md
+* https://developer.apple.com/documentation/xcodekit?language=objc
+* https://www.vadimbulavin.com/xcode-source-editor-extension-tutorial/
+* https://nshipster.com/xcode-source-extensions/
+
+Examples
+* https://github.com/bow-swift/nef-plugin
 
--- setup.sh
+++ setup.sh
@@ -38,27 +38,38 @@ fi
 echo "Copying the IDE Plug-in to the plug-ins directory"
 cp -r Plug-ins/Objective-J.ideplugin $plugins_dir
 
+# Create Specifications directory if it doesn't exist
+specifications_dir=~/Library/Developer/Xcode/Specifications/
+if [ ! -d "$specifications_dir" ]; then
+    echo "Creating specifications directory at $specifications_dir"
+	mkdir $specifications_dir
+fi
+
+# Copy the IDE Plugin to the plug-ins directory
+echo "Copying the language specification to the specifications directory"
+cp -r Specifications/Objective-J.xclangspec $specifications_dir
+
 # Get the selected Xcode.app's path
-xcode_path=$(xcode-select -p)
-xcode_path=$(dirname $xcode_path)
+#xcode_path=$(xcode-select -p)
+#xcode_path=$(dirname $xcode_path)
 
 # Get Specifications directory
-spec_dir="${xcode_path}/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications"
+#spec_dir="${xcode_path}/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageSpecifications"
 
 # Copy the language specification to the specs directory
-echo "Copying the language specificaiton to $spec_dir"
-cp Specifications/Objective-J.xclangspec $spec_dir
+#echo "Copying the language specification to $spec_dir"
+#cp Specifications/Objective-J.xclangspec $spec_dir
 
 # Xcode build specs file
-cp Specifications/Objective-J.xcspec $spec_dir
+#cp Specifications/Objective-J.xcspec $spec_dir
 
 # Get language metadata directory
-metadata_dir="${xcode_path}/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata"
+#metadata_dir="${xcode_path}/SharedFrameworks/SourceModel.framework/Versions/A/Resources/LanguageMetadata"
 
 # Copy the source code language plist to the metadata directory
-echo "Copying the source code language plist to $metadata_dir"
-cp Xcode.SourceCodeLanguage.Objective-J.plist $metadata_dir
+#echo "Copying the source code language plist to $metadata_dir"
+#cp Xcode.SourceCodeLanguage.Objective-J.plist $metadata_dir
 
-defaults read ${xcode_path}/Info DVTPlugInCompatibilityUUID
+#defaults read ${xcode_path}/Info DVTPlugInCompatibilityUUID
 
 echo "Please restart Xcode"
